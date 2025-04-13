c072e0397671563497338c95ca94ca926ec6643c
RARI <65898240+ERDENE-Ariunbilig@users.noreply.github.com>
2025-04-13 19:11:40 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Elixir/',
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
   build: {
     minify: 'terser',
     cssMinify: true,
