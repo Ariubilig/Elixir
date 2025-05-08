9f428053083a569fe65623d05fc647445dbc5d95
RARI <65898240+ERDENE-Ariunbilig@users.noreply.github.com>
2025-05-08 20:24:13 +0800

    local, deploy error fixxxx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const baseUrl = "/Elixir/";
+const baseUrl = window.location.hostname === 'localhost' || window.location.hostname.includes('127.0.0.1') 
+  ? '/' : "/Elixir/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
